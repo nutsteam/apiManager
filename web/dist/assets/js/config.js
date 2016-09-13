584f0eb2a1fe2c5a27be4dd9b8158b5211a248a3
周靖杰 <org.zhoujingjie@gmail.com>
2016-09-13 23:21:57 +0800

    no message

--- web/dist/assets/js/config.js
+++ web/dist/assets/js/config.js
@@ -2,7 +2,7 @@ toastr.options.escapeHtml = true;
 toastr.options.closeButton = true;
 toastr.options.positionClass = 'toast-top-center';
 toastr.options.preventDuplicates=true;
-window.root='//www.xiaoyaoji.com.cn/api';
+window.root='//localhost/api';
 window.ctx='';
 window._vue_={
     history:false
